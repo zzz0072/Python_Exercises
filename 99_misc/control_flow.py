82471de472b044319f94b946564df6e74509ed1d
Wen.Liao <zzz00072@gmail.com>
2014-04-17 13:42:19 +0800

    Test functions

--- 99_misc/control_flow.py
+++ 99_misc/control_flow.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+# function
+def sum(op1, op2):
+    return op1 + op2
+
+print sum(1, 2)
+print sum("I am ", "zzz");
+
+# pass
 print "press ctrl + c to continue"
 
 while True:
