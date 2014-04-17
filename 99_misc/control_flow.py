441eac1b7d9d3f2fb30fbd2faf1dbe8fe3908402
Wen.Liao <zzz00072@gmail.com>
2014-04-17 14:02:25 +0800

    Test default value of a function

--- 99_misc/control_flow.py
+++ 99_misc/control_flow.py
@@ -7,6 +7,15 @@ my_sum = sum
 print my_sum(1, 2)
 print my_sum("I am ", "zzz");
 
+# Default value in a fuction
+init = 12
+def accumulate(val = init):
+    val += val
+    return val
+
+my_accu = accumulate
+init = 11
+print my_accu() # is 12 + 12 rather than 11 + 11
 
 # pass
 print "press ctrl + c to continue"
