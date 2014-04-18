d0b72c81e30f1c8b7c270f558ea524a4dc8f70fc
Wen.Liao <zzz00072@gmail.com>
2014-04-18 14:05:40 +0800

    Test !r and !s in print format

--- 03_IO/io.py
+++ 03_IO/io.py
@@ -11,3 +11,11 @@ for i in range(1, 21):
         print(repr(i).rjust(2)),  # , means no new line
         print(repr(j).rjust(2)),
         print(repr(i*j).rjust(4)) # This do not have ,
+
+# Test print arguments
+print("{i} x {j} = {k}".format(i = 9, j = 9, k = 81))
+
+# Test !s and !r
+import math
+res = math.sqrt(3)
+print("sqrt(3) is str: {!s} and repr: {!r}".format(res, res))
