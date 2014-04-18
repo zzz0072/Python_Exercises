ab7fe149b197fe6a0d4d91a2899c1adc61374659
Wen.Liao <zzz00072@gmail.com>
2014-04-18 10:25:31 +0800

    Test run a module alone

--- 02_module/module.py
+++ 02_module/module.py
@@ -4,3 +4,14 @@ def my_sum(x, y):
 
 def my_sum2(x, y):
     return x + y
+
+# To run method alone
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        print("%s str1 str2" % sys.argv[0])
+        raise SystemExit(1)
+
+    print(my_sum(sys.argv[1], sys.argv[2]))
+
+
