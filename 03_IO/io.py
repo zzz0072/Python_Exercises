d24a6ca7c5981c7bdb40d689d12d57d61615374c
Wen.Liao <zzz00072@gmail.com>
2014-04-18 12:19:28 +0800

    To string without new line test and rjust alignment for pretty format

--- 03_IO/io.py
+++ 03_IO/io.py
@@ -3,3 +3,11 @@
 for i in range(1, 21):
     for j in range(1, 21):
         print("{0:2d} x {1:2d} = {2:4d}".format(i, j, (i * i)))
+
+# To string without new line test and rjust alignment for pretty format
+print("To string without new line test and rjust alignment for pretty format")
+for i in range(1, 21):
+    for j in range(1, 21):
+        print(repr(i).rjust(2)),  # , means no new line
+        print(repr(j).rjust(2)),
+        print(repr(i*j).rjust(4)) # This do not have ,
