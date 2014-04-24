fa36fc3301e7db47d72d0cd7c47bddf30cd7719d
Wen.Liao <zzz00072@gmail.com>
2014-04-24 11:23:31 +0800

    Test unit test constructor and destructor

--- 06_test/unit_test_func.py
+++ 06_test/unit_test_func.py
@@ -3,8 +3,15 @@ import unittest
 from my_calc import my_add
 class test_func(unittest.TestCase):
     def test_my_add(self):
+        print("Test begin")
         res = my_add(1, 2)
         self.assertEqual(res, 3)
+
+    def setUp(self):
+        print("Setup")
+
+    def tearDown(self):
+        print("Tear down")
 
 if __name__ == "__main__":
     unittest.main()
